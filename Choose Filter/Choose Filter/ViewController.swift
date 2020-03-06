e56d01e840e808194d1c534f17b9083caa799cc1
Ariana Kalili <arianakalili20@k12.xrds.org>
2020-03-06 09:56:29 -0800

    flipped order of filter app

--- Choose Filter/Choose Filter/ViewController.swift
+++ Choose Filter/Choose Filter/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
     var filterType = "a thing"
-    
+    var image =  UIImage()
 
     @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
@@ -21,62 +21,72 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
   func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
          self.dismiss(animated: true, completion: nil)
-         let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
-       //  imageView.image = image
-         let inputImage = image
-         let context = CIContext(options: nil)
-         if let filter = CIFilter(name: filterType) {
-             let beginImage = CIImage(image: inputImage)
-             filter.setDefaults()
-             filter.setValue(beginImage, forKey: kCIInputImageKey)
-             if let output = filter.outputImage{
-                 if let cgimg = context.createCGImage(output,from: output.extent){
-                     let processedImage = UIImage(cgImage: cgimg)
-                     imageView.image = processedImage
-                 }
-             }
-         }
+         image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+    imageView.image = image
     
      }
-    @IBAction func sepia(_ sender: Any) {
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary){
-            filterType = "CISepiaTone"
+    
+    @IBAction func takePhoto(_ sender: Any) {
+    if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary){
     
                 let imagePicker = UIImagePickerController()
                 imagePicker.delegate = self
-                //  for phone
-                //  imagePicker.sourceType = .camera
-                //  for simulator
+        
                 imagePicker.sourceType = .photoLibrary
 
                 self.present(imagePicker, animated: true, completion: nil)
             }
+    }
+    @IBAction func sepia(_ sender: Any) {
+      
+            let inputImage = image
+              let context = CIContext(options: nil)
+              if let filter = CIFilter(name: "CISepiaTone") {
+                  let beginImage = CIImage(image: inputImage)
+                  filter.setDefaults()
+                  filter.setValue(beginImage, forKey: kCIInputImageKey)
+                  if let output = filter.outputImage{
+                      if let cgimg = context.createCGImage(output,from: output.extent){
+                          let processedImage = UIImage(cgImage: cgimg)
+                          imageView.image = processedImage
+                      }
+                  }
+              }
         
     }
     
     
     @IBAction func Noir(_ sender: Any) {
-  if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary){
-    filterType = "CIPhotoEffectNoir"
-      let imagePicker = UIImagePickerController()
-      imagePicker.delegate = self
-      imagePicker.sourceType = .photoLibrary
-
-      self.present(imagePicker, animated: true, completion: nil)
-  }
+  let inputImage = image
+  let context = CIContext(options: nil)
+  if let filter = CIFilter(name: "CIPhotoEffectNoir") {
+      let beginImage = CIImage(image: inputImage)
+      filter.setDefaults()
+      filter.setValue(beginImage, forKey: kCIInputImageKey)
+      if let output = filter.outputImage{
+          if let cgimg = context.createCGImage(output,from: output.extent){
+              let processedImage = UIImage(cgImage: cgimg)
+              imageView.image = processedImage
+          }
+      }
     }
     
-    
+}
    @IBAction func fade(_ sender: Any) {
-    if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary){
-        filterType = "CIPhotoEffectFade"
-                let imagePicker = UIImagePickerController()
-                imagePicker.delegate = self
-                imagePicker.sourceType = .photoLibrary
-
-                self.present(imagePicker, animated: true, completion: nil)
+   let inputImage = image
+    let context = CIContext(options: nil)
+    if let filter = CIFilter(name: "CIPhotoEffectFade") {
+        let beginImage = CIImage(image: inputImage)
+        filter.setDefaults()
+        filter.setValue(beginImage, forKey: kCIInputImageKey)
+        if let output = filter.outputImage{
+            if let cgimg = context.createCGImage(output,from: output.extent){
+                let processedImage = UIImage(cgImage: cgimg)
+                imageView.image = processedImage
             }
-    }
+        }
+      }
     
+    }
 }
 
